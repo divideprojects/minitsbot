25f0e1f308f47eb0209d2350175deff81480e812
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-05-28 01:04:48 +0530

    Done

--- main.ts
+++ main.ts
@@ -6,11 +6,11 @@ import { apiThrottler } from "https://deno.land/x/grammy_transformer_throttler@v
 const bot = new Bot(String(Deno.env.get("TOKEN")));
 bot.api.config.use(apiThrottler());
 
-bot.command("start", (ctx) => {
+bot.command("start", async (ctx) => {
     if (ctx.chat.type === "private") {
-        return ctx.reply("Welcome! just add me in any chat with ban, delete and invite permission and i will do the rest; By @Memers_Gallery")
+        return await ctx.reply("Welcome! just add me in any chat with ban, delete and invite permission and i will do the rest; By @Memers_Gallery")
     } else {
-        return ctx.reply("I'm alive!")
+        return await ctx.reply("I'm alive!")
     }
 });
 bot.on("message", async (ctx) => {
@@ -18,10 +18,9 @@ bot.on("message", async (ctx) => {
       return await ctx.deleteMessage()
   };
   if (ctx.from.id === 136817688 && ctx.chat.type === "supergroup") {
-      console.log("kk")
       return await ctx.deleteMessage()
 }});
-bot.on("chat_join_request", (ctx) => ctx.approveChatJoinRequest(ctx.from.id));
+bot.on("chat_join_request", async (ctx) => await ctx.approveChatJoinRequest(ctx.from.id));
 bot.catch((err) => {
   const e = err.error;
   if (e instanceof GrammyError) {
