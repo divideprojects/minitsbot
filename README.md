8fd51f5d6ee4d5c788d03836c4db08e00a6d3d48
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-05-28 21:09:43 +0530

    Webhook. (#3)

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 
 ### Deploy to Heroku
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/DivideProjects/minitsbot.git)
+
+#### Note:
+After deploying click on this by replacing your bot token and heruko app name:
+```
+https://api.telegram.org/bot<token>/setWebhook?url=https://<app_name>.herokuapp.com/<token>
+```
--- main.ts
+++ main.ts
@@ -1,33 +1,61 @@
-import "https://deno.land/x/dotenv/load.ts";
-import { Bot, GrammyError, HttpError } from "https://deno.land/x/grammy@v1.8.3/mod.ts";
-import { run } from "https://deno.land/x/grammy_runner@v1.0.3/mod.ts";
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
+import {
+    Bot,
+    GrammyError,
+    HttpError,
+    webhookCallback,
+} from "https://deno.land/x/grammy@v1.8.3/mod.ts";
 import { apiThrottler } from "https://deno.land/x/grammy_transformer_throttler@v1.1.2/mod.ts";
 
-const bot = new Bot(String(Deno.env.get("TOKEN")));
+const TOKEN = String(Deno.env.get("TOKEN"));
+const vport = Number(Deno.env.get("PORT"));
+const bot = new Bot(TOKEN);
 bot.api.config.use(apiThrottler());
+const handleUpdate = webhookCallback(bot, "std/http");
 
 bot.command("start", async (ctx) => {
     if (ctx.chat.type === "private") {
-        return await ctx.reply("Welcome! just add me in any chat with delete and invite permission and i will do the rest.")
+        return await ctx.reply(
+            "Welcome! just add me in any chat with delete and invite permission and i will do the rest.",
+        );
     } else {
-        return await ctx.reply("I'm alive!")
+        return await ctx.reply("I'm alive!");
     }
 });
 bot.on("message", async (ctx) => {
-  if (ctx.from.id === 136817688 && ctx.chat.type === "supergroup") {
-      return await ctx.deleteMessage()
-}});
-bot.on("chat_join_request", async (ctx) => await ctx.approveChatJoinRequest(ctx.from.id));
+    if (ctx.from.id === 136817688 && ctx.chat.type === "supergroup") {
+        return await ctx.deleteMessage();
+    }
+});
+bot.on(
+    "chat_join_request",
+    async (ctx) => await ctx.approveChatJoinRequest(ctx.from.id),
+);
 bot.catch((err) => {
-  const e = err.error;
-  if (e instanceof GrammyError) {
-    console.error("Error in request:", e.description);
-  } else if (e instanceof HttpError) {
-    console.error("Could not contact Telegram:", e);
-  } else {
-    console.error("Unknown error:", e);
-  }
+    const e = err.error;
+    if (e instanceof GrammyError) {
+        console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+        console.error("Could not contact Telegram:", e);
+    } else {
+        console.error("Unknown error:", e);
+    }
 });
 
 console.log("Started!");
-run(bot);
+serve({
+    ["/" + TOKEN]: async (req) => {
+        if (req.method == "POST") {
+            try {
+                return await handleUpdate(req);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        return new Response();
+    },
+    "/": () => {
+        return new Response("Ok!");
+    },
+}, { port: vport });
